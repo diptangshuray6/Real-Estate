af6eb9ff3d5cfda771af2193c881e49c263e6ccd
Diptangshu Ray <diptangshuray617@gmail.com>
2024-07-30 23:00:48 +0530

    module creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,22 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
 export default function App() {
-  return <h1 className="text--500 underline">Hello world!</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
